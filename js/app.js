5aaf43997e08df78109af100be75b08bd0b191fa
publish-dt <119054497+publish-dt@users.noreply.github.com>
2024-10-15 16:29:56 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -14,87 +14,116 @@ SUAMVOU7IIH2XY4Z3HK77CGLC6EHIGSZITHS77B6AJKK24IGXUWIZKOI4U
 *************************************************************
 `;
 
+var isWS = true; // false
 
-if(nats_core === undefined || nats_core === null) alert("Ваш браузер устарел, и не может поддерживать данное приложение.");
-else {
-        
 (async () => {
 
-    const nc = await nats_core.wsconnect({
-        servers: "wss://connect.ngs.global", //  ws://localhost:8080 wss://demo.nats.io:8443 tls://connect.ngs.global
-        authenticator: nats_core.credsAuthenticator(new TextEncoder().encode(creds)),
-        //user: "client",
-        //pass: "12345",
-        name: "Client-Web"
-    });
+    if (nats_core === undefined || nats_core === null) {
+        isWS = false;
+        alert("Ваш браузер устарел, и не может поддерживать данное приложение.");
+    }
+    else {
+        //try {
+        var nc = await nats_core.wsconnect({
+            servers: "wss://connect.ngs.global", //  ws://localhost:81 wss://demo.nats.io:8443 tls://connect.ngs.global
+            authenticator: nats_core.credsAuthenticator(new TextEncoder().encode(creds)),
+            //user: "client",
+            //pass: "12345",
+            name: "Client-Web"
+        });
+        /*} catch (_err) {
+            console.log(`error connecting to ${JSON.stringify(_err.message)}`);
+        }*/
 
-    console.log(`connected to ${nc.getServer()}`);
+        console.log(`connected to ${nc.getServer()}`);
 
-    const sc = new nats_core.StringCodec();
-    const jc = new nats_core.JSONCodec();
+        var sc = new nats_core.StringCodec();
+        var jc = new nats_core.JSONCodec();
 
+    }
+        boost.onclick = function (event) {
+            if (event.target.nodeName != 'A') return;
 
-    boost.onclick = function (event) {
-        if (event.target.nodeName != 'A') return;
+            let href = event.target.getAttribute('href');
+            //alert(href); // может быть подгрузка с сервера, генерация интерфейса и т.п.
+            getData(href);
 
-        let href = event.target.getAttribute('href');
-        alert(href); // может быть подгрузка с сервера, генерация интерфейса и т.п.
-        getData(href);
+            return false; // отменить действие браузера (переход по ссылке)
+        };
 
-        return false; // отменить действие браузера (переход по ссылке)
-    };
+        window.addEventListener("unload", async function (e) {
 
-    window.addEventListener("unload", async function (e) {
+            const done = nc.closed(); // nc.drain(); // drain используется когда есть подписки, чтобы их корректно завершить. И close уже не надо.
+            await nc.close();
+            const err = await done;
+            if (err) {
+                console.log(`error closing:`, err);
+            }
 
-        const done = nc.closed(); // nc.drain(); // drain используется когда есть подписки, чтобы их корректно завершить. И close уже не надо.
-        await nc.close();
-        const err = await done;
-        if (err) {
-            console.log(`error closing:`, err);
-        }
+            //return "Do you really want to close?";  //Webkit, Safari, Chrome
+        });
 
-        //return "Do you really want to close?";  //Webkit, Safari, Chrome
-    });
+        async function getData(url) {
 
-    async function getData(url) {
-
-        try {
-            //const data = nats_core.JSONCodec((this_, key, value) => { return value; }).encode({"p": 5});
-            const data = jc.encode({ name: "Alex", path: url }); // ["hello", 5] sc.encode(String(4))
             try {
-                rep = await nc.request("MSU.Req.Main", data, { timeout: 5000 });
-                //const numbers = nats_core.JSONCodec<number[]>().decode(rep.data);
-                var res = jc.decode(rep.data);
-                if (res.error == undefined) {
-                    console.log(`Данные от сервиса получены.`);
+                var res = undefined;
+                var dataResp = undefined;
+
+                if (isWS) {
+                    //const data = nats_core.JSONCodec((this_, key, value) => { return value; }).encode({"p": 5});
+                    var dataReq = jc.encode({ name: "Alex", path: url }); // ["hello", 5] sc.encode(String(4))
+
+                    var response = await nc.request("MSU.Req.Main", dataReq, { timeout: 5000 }); // MSU.Req.Main
+                    //const numbers = nats_core.JSONCodec<number[]>().decode(response.data);
+                    res = jc.decode(response.data);
+                    if (res.code == undefined && res.result != undefined) {
+                        console.log(`Данные от сервиса получены.`);
+                        dataResp = res.result.model;
+                    }
+                    else console.log(`Произошла ошибка в сервисе: ${res.code}, ${res.message}`);
+                }
+                else {
+                    var response = await fetch("http://2.56.88.201" + url); // http://localhost:8888 http://localhost:8080/api/MSU/Main?path=
+                    if (response.ok) { // если HTTP-статус в диапазоне 200-299
+                        console.log(`Данные от сервиса получены.`);
+                        //res0 = await response.text();
+                        res = await response.json();
+                        //console.log(`Получены данные: ${res.content}`);
+                        dataResp = res;
+                    } else {
+                        alert("Ошибка HTTP: " + response.status);
+                    }
+                }
+
+                if (dataResp !== undefined) {
+
+                    var el_content = document.querySelector("#Content");
 
                     /*var el_title = document.querySelector("#Title");
                     el_title.innerHTML = res.Name;
-
+    
                     var el_description = document.querySelector("#Description");
                     el_description.innerHTML = res.Description;*/
 
-                    var el_content = document.querySelector("#Content");
-                    el_content.innerHTML = res.content;
-                    window.scroll(0, 0);
+                    if (dataResp.content !== undefined) {
+                        el_content.innerHTML = dataResp.content;
+
+                        //console.log(`Отображены данные: ${data.content}`);
+                        window.scroll(0, 0);
+                    }
                 }
-                else
-                    console.log(`Произошла ошибка в сервисе: ${res.error}`);
             } catch (ex) {
                 if (ex.code == "503")
                     console.log(`Сервис для ответа не запущен. Ошибка: ${JSON.stringify(ex)}`);
                 else if (ex.code == "TIMEOUT")
                     console.log(`Сервис долго не отвечает. Ошибка: ${JSON.stringify(ex)}`);
                 else
-                    console.log(`Ошибка от сервиса: ${JSON.stringify(ex)}`);
+                    console.log(`Ошибка от сервиса: ${ex}`);
             }
 
 
-        } catch (_err) {
-            console.log(`error connecting to ${JSON.stringify(_err.message)}`);
+
+            return 4;
         }
 
-        return 4;
-    }
 })();
-}
