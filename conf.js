ddb396bd423f6d2c38963e1d4c58e89f0ed770b9
publish-dt <119054497+publish-dt@users.noreply.github.com>
2025-01-20 16:42:55 +0300

    Add files via upload

--- conf.js
+++ conf.js
@@ -1,4 +1,2 @@
-let prefix = "";
-let hostname = "https://2.56.88.201:8083";
-const staticBase = "https://cdn.jsdelivr.net/gh/publish-dt/msu-otk@main";
-const isStaticMode = true;
+hostname = "https://2.56.88.201:8083";
+isStaticMode = true;
